ee7511f1f7aa748b1b89c79932cef96c684afcea
Best-code <cpmraygun@gmail.com>
2025-03-11 03:13:52 -0400

    Year working

--- Src/GUI/app.py
+++ Src/GUI/app.py
@@ -19,16 +19,17 @@ class MainApp(QMainWindow):
         self.year = 2025
         self.con = sqlite3.connect("/Users/melons/Documents/Code/Python/HOR-Trades/Src/Data-Collection-Processing/Finance.db")
         self.cur = self.con.cursor()
-        self.tableName = f"trade_{self.year}"
         
         self.name = ""
         self.ticker = ""
+        self.year = self.ui.DE_Year.date().toString("yyyy")
         self.after = ""
         self.before = ""
 
         self.ui.LE_LastName.textChanged.connect(self.onNameUpdate)
         self.ui.LE_Ticker.textChanged.connect(self.onTickerUpdate)
         self.ui.DE_After.dateChanged.connect(self.onAfterUpdate)
+        self.ui.DE_Year.dateChanged.connect(self.onYearUpdate)
         self.ui.DE_Before.dateChanged.connect(self.onBeforeUpdate)
         self.ui.B_Search.clicked.connect(self.executeSQL)
 
@@ -38,13 +39,27 @@ class MainApp(QMainWindow):
     def onTickerUpdate(self):
         self.ticker = self.ui.LE_Ticker.text()  # Update Name text
 
+    def onYearUpdate(self):
+        self.year = self.ui.DE_Year.date().toString("yyyy")
+
     def onAfterUpdate(self):
         self.after = self.ui.DE_After.date()
 
     def onBeforeUpdate(self):
         self.before = self.ui.DE_Before.date()
 
+
+    """
+    TODO!
+    
+    1. Simplify the date range to just one year at a time for now between 2023-2025
+    2. If their is a name filter, we will need to join Person x Trade together on ALL.
+    3. Check the date range and pull info from those tables joined together.
+    
+    """
+
     def generateQuery(self):
+        self.tableName = f"trade_{self.year}"
         baseQuery = f"SELECT * FROM {self.tableName}"
         # If there is nothing, then search everything
         
